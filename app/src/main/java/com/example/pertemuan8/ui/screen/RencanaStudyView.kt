c846809ead957d6c7b960e7c2e6f4cd1d0e2aba0
Jantopthegreat <fauzangriya@gmail.com>
2024-11-22 18:42:25 +0700

    3: Menambahkan layout row di dalam layout column lalu menyesuaikan tampilan tata letaknya

--- app/src/main/java/com/example/pertemuan8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/screen/RencanaStudyView.kt
@@ -1,12 +1,20 @@
 package com.example.pertemuan8.ui.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import com.example.pertemuan8.model.Mahasiswa
 import com.example.pertemuan8.model.RencanaStudi
+import androidx.compose.ui.Modifier
 
 @Composable
 fun RencanaStudyView(
@@ -19,5 +27,21 @@ fun RencanaStudyView(
     var pilihanKelas by remember { mutableStateOf("") }
 
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+        }
+    }
 }
 
