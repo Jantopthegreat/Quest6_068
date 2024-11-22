d81d1e060fd650bb9e7e4fd67b5ea5d258ae6d1a
Jantopthegreat <fauzangriya@gmail.com>
2024-11-22 19:50:52 +0700

    17: Membuat function MahasiswaApp dengan parameter mod

--- app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
@@ -2,11 +2,23 @@ package com.example.pertemuan8.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.pertemuan8.ui.view.viewmodel.MahasiswaViewModel
+import com.example.pertemuan8.ui.view.viewmodel.RencanaStudyViewModel
 
 enum class Halaman {
     Splash,
     Mahasiswa,
     Matakuliah,
     Tampil,
+}
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
 }
