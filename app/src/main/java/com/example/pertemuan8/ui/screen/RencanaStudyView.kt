315c04376cb1834e480240c535f585019e6b2a08
Jantopthegreat <fauzangriya@gmail.com>
2024-11-22 18:46:59 +0700

    4: Menambahkan elemen image lalu mengatur resource nya serta bentuk dan ukuran gambarnya

--- app/src/main/java/com/example/pertemuan8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/screen/RencanaStudyView.kt
@@ -1,10 +1,13 @@
 package com.example.pertemuan8.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +18,9 @@ import androidx.compose.ui.unit.dp
 import com.example.pertemuan8.model.Mahasiswa
 import com.example.pertemuan8.model.RencanaStudi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import com.example.pertemuan8.R
 
 @Composable
 fun RencanaStudyView(
@@ -40,7 +46,13 @@ fun RencanaStudyView(
                 .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ){
-
+            Image(
+                painter = painterResource(id = R.drawable.images),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
         }
     }
 }
